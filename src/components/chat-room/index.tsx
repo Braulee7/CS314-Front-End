9bce93396b2f27cb5d4066ae2bd05f7c64309f1d
Braulee7 <braulio00001@gmail.com>
2024-05-08 06:23:07 -0700

    Added responsive elements

--- src/components/chat-room/index.tsx
+++ src/components/chat-room/index.tsx
@@ -1,7 +1,7 @@
 function ChatRoom() {
   return (
     <>
-      <div className="w-[90%] bg-indigo-900 bg-opacity-20 backdrop-blur-lg rounded dr -z-10">
+      <div className="w-[90%] lg:w-[76vw] bg-indigo-900 bg-opacity-20 backdrop-blur-lg rounded dr -z-10">
         <h1>Chat Room</h1>
       </div>
     </>
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,19 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Search from "../search";
 
 function Navbar() {
   const [show, setShow] = useState(false);
+  const [screen_width, setScreenWidth] = useState(window.innerWidth);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  });
   return (
     <div className="flex flex-col justify-between h-screen w-[10vw] bg-gray-500">
       <button
-        className="top-0 left-0 p-2 h-15 mb-2 bg-gray-800 text-white z-10 w-fit-content"
+        className="top-0 left-0 p-2 h-15 mb-2 bg-gray-800 text-white z-10 w-fit-content lg:hidden"
         onClick={() => setShow(!show)}
       >
         ☰
       </button>
-      {show && (
-        <div className="fixed w-200 h-screen p-5 pt-14 bg-gray-500">
+      {(show || screen_width >= 1024) && (
+        <div className="fixed lg:w-[24vw] w-200 h-screen p-5 pt-14 bg-gray-500">
           <Search />
           <h1>Create Group</h1>
           <br />
