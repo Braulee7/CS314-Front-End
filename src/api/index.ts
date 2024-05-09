413dc9c621f3e22636485507f027a0e6d07dd8db
Braulee7 <braulio00001@gmail.com>
2024-05-09 12:18:44 -0700

    Added better comments explaining the class

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,14 +1,22 @@
 import { JwtPayload, jwtDecode } from "jwt-decode";
 import { redirect } from "react-router-dom";
 
+// interface of what a user has
+// currently is just a username
 export interface UserObj {
   username: string;
 }
 
+// interace for data held by our API class
 interface ApiObj {
   username: string;
   accessToken: string;
 }
+
+// SINGLETON CLASS
+// API class will handle all fetching for the user. It will
+// contain the authenticating information (usrname and JWT access
+// token) and will be used to make all requests to the backend.
 
 class Api {
   // save the username of the user and
@@ -20,6 +28,9 @@ class Api {
 
   // attempts to log in the user and returns the instance of
   // api if successful, throws an error otherwise
+  // @param username {string}: the username of the user
+  // @param password {string}: the password of the user
+  // @return {Api}: the instance of the api
   public static async Login(username: string, password: string): Promise<Api> {
     const user = { username: username, password: password };
     // POST request to the backend
@@ -56,6 +67,9 @@ class Api {
 
   // attempts to create a new user and log them in if successful
   //returns the instance of api if successful, throws an error otherwise
+  // @param username {string}: the username of the user
+  // @param password {string}: the password of the user
+  // @return {Api}: the instance of the api
   public static async CreateUser(
     username: string,
     password: string
@@ -94,6 +108,7 @@ class Api {
 
   // attempts to get new credentials using refresh token
   // returns the instance of api if successful, throws an error otherwise
+  // @return {Promis<API>} the instance of the api
   public static async Refresh(): Promise<Api> {
     // get the token and username
     const { username, accessToken } = await Api.RefreshToken();
@@ -104,6 +119,8 @@ class Api {
 
   // searches the database for any users with the prefix
   // of the passed in username and returns a list of users
+  // @param username {string}: the prefix of the username to search for
+  // @return {Promise<UserObj[]>}: the list of users retrieved
   public async SearchForUsers(username: string): Promise<UserObj[]> {
     // set up the query parameters
     const url = "http://localhost:3333/user?username=" + username;
@@ -136,6 +153,8 @@ class Api {
   }
 
   // checks if a room exists between the current logged in user and the other user
+  // @param other_user {string}: the other user to check if a room exists
+  // @return {Promise<number>}: the room id if it exists, -1 otherwise
   public async CheckRoomExists(other_user: string): Promise<number> {
     // set up url
     const url = `http://localhost:3333/room?user1=${this._username}&user2=${other_user}`;
@@ -168,10 +187,17 @@ class Api {
     }
   }
 
+  // getter function to retrieve the username of the currently
+  // logged in user
+  // @return {string}: the username of the user
   public get username(): string {
     return this._username;
   }
 
+  // getter function to retrieve the instance of the currently
+  // logged in user API.
+  // @return {Api | null}: the instance of the user API if a user is
+  //  logged in, null otherwise
   public static get User(): Api | null {
     if (!this._instance) {
       return null;
@@ -179,6 +205,9 @@ class Api {
     return this._instance;
   }
 
+  // static function to refresh the access token by making a
+  // a request to the backend with our cookies credentials
+  // @return {Promise<ApiObj>}: the new access token and username
   private static async RefreshToken(): Promise<ApiObj> {
     const response = await fetch("http://localhost:3333/auth/refresh", {
       method: "POST",
@@ -204,6 +233,7 @@ class Api {
   }
 
   // checks if the access token is expired and returns a valid token
+  // @return {string}: a valid access token
   private get AccessToken(): string {
     // check expiration
     const decoded_token: JwtPayload = jwtDecode(this._accessToken);
@@ -221,13 +251,17 @@ class Api {
     return this._accessToken;
   }
 
+  // DATA MEMBERS
+  // static instance of the user, the singleton
   private static _instance: Api;
+  // username and access token of the user
   private _username: string;
   private _accessToken: string;
 }
 
 // loader to check if a user is logged in
 export function loader() {
+  // get the instance of the user
   let user = Api.User;
   if (!user) {
     // attempt to get new tokens via refresh token in cookies
