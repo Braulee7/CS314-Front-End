d82517c6ff6741590c02ae7d877ba249f2954ab6
Braulee7 <braulio00001@gmail.com>
2024-05-09 12:06:49 -0700

    Fixed failing test

--- src/components/login/index.test.tsx
+++ src/components/login/index.test.tsx
@@ -1,5 +1,12 @@
 import { render } from "@testing-library/react";
+import { useNavigate } from "react-router-dom";
 import Login from ".";
+
+// Mock the navigate function to ensure the test
+// does not fail due to the use of the hook
+vi.mock("react-router-dom", () => ({
+  useNavigate: vi.fn(),
+}));
 
 describe("Login form", () => {
   test("It Renders correctly", () => {
