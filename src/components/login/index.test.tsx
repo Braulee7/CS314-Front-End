a8e80a4c72fc86d1424f2047da871a0a5e1fa2b6
Braulee7 <braulio00001@gmail.com>
2024-05-20 16:43:37 -0700

    Fixed tests for login form

--- src/components/login/index.test.tsx
+++ src/components/login/index.test.tsx
@@ -1,14 +1,25 @@
+import { userEvent } from "@testing-library/user-event";
 import { render } from "@testing-library/react";
 import { useNavigate } from "react-router-dom";
-import Login from ".";
+import Api from "../../api";
+import Login from "./index";
 
 // Mock the navigate function to ensure the test
 // does not fail due to the use of the hook
+Api.Login = vi.fn().mockImplementation(async (user, password) => {
+  console.log(`Logging in user: ${user} with password: ${password}`);
+  return Promise.resolve();
+});
+
 vi.mock("react-router-dom", () => ({
   useNavigate: vi.fn(),
 }));
 
 describe("Login form", () => {
+  beforeAll(() => {
+    vi.resetAllMocks();
+  });
+
   test("It Renders correctly", () => {
     // ARRANGE
     const { getByLabelText } = render(<Login />);
@@ -19,5 +30,25 @@ describe("Login form", () => {
     // ASSERT
     expect(username_input).toBeDefined();
     expect(password_input).toBeDefined();
+  });
+
+  test("makes a request to the server", async () => {
+    // ARRANGE
+    const { getByTestId } = render(<Login />);
+    const username_input = getByTestId("username-input");
+    const password_input = getByTestId("password-input");
+    const submit_button = getByTestId("submit-button");
+
+    // set up the mock user
+    userEvent.setup();
+
+    // ACT
+    // type into the input fields
+    await userEvent.type(username_input, "user");
+    await userEvent.type(password_input, "password");
+    // click the submit button
+    await userEvent.click(submit_button);
+    // ASSERT
+    expect(Api.Login).toHaveBeenCalledWith("user", "password");
   });
 });
