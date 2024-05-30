7b87bd6ec14c302ab6d6f859b85dd7bdf7b949dd
Braulee7 <braulio00001@gmail.com>
2024-05-30 11:18:29 -0700

    Added scrolling to the most recent message for better UX

--- src/components/message-list/index.tsx
+++ src/components/message-list/index.tsx
@@ -1,15 +1,27 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import Message from "../message";
 import { WebSocketContext } from "../../context/socket-context";
 
 function MessageList() {
   const { messages } = useContext(WebSocketContext);
+  const recently_sent_message_ref = useRef<HTMLLIElement>(null);
+
+  useEffect(() => {
+    if (recently_sent_message_ref.current) {
+      recently_sent_message_ref.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   return (
-    <div className="h-[95vh]">
-      <ul className="flex flex-col overflow-y-scroll scrollbar ">
-        {messages.map((message) => (
-          <li key={message.message_id}>
+    <div className="h-[95vh] overflow-y-scroll scrollbar">
+      <ul className="flex flex-col ">
+        {messages.map((message, index) => (
+          <li
+            key={message.message_id}
+            ref={
+              index === messages.length - 1 ? recently_sent_message_ref : null
+            }
+          >
             <Message
               content={message.message}
               sender={message.sending_user}
