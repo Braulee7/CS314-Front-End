681cd608215d8d3e81ec4d064198b942ea966cc3
Braulee7 <braulio00001@gmail.com>
2024-06-04 15:27:59 -0700

    Refactored search component to be more versatile

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import Search from "../search";
 import RoomList from "../room-list";
 import Logout from "../logout";
@@ -9,6 +9,7 @@ function Navbar() {
   const [show, setShow] = useState(false);
   const [screen_width, setScreenWidth] = useState(window.innerWidth);
   const { user } = useLoaderData() as { user: Api };
+  const nav = useNavigate();
   const screen_width_threshold = 1080;
 
   useEffect(() => {
@@ -19,26 +20,41 @@ function Navbar() {
     return () => window.removeEventListener("resize", handleResize);
   });
 
+  const createDmRoom = async (username: string) => {
+    try {
+      let room_id = await user.CheckRoomExists(username);
+      if (room_id === -1) {
+        // create a new room and redirect to it
+        room_id = await user.createDirectMessageRoom(username);
+      }
+      nav(`/rooms/${room_id}`);
+    } catch (e) {
+      // TODO: Add error message to UI
+      console.log(e);
+    }
+  };
+
   return (
-    <div className="flex flex-col justify-between h-screen w-full max-w-[25vw] lg:max-w-[300px] bg-gray-500">
-      {screen_width < screen_width_threshold && (
-        <button
-          className="top-0 left-0 p-2 h-15 mb-2 bg-gray-800 text-white z-50 w-fit-content"
-          onClick={() => setShow(!show)}
-        >
-          ☰
-        </button>
-      )}
-      {(show || screen_width >= screen_width_threshold) && (
-        <div className="fixed lg:w-[24vw] w-200 max-w-64 h-screen p-5 pt-14 bg-gray-500 z-20">
-          <Search />
-          <h1>Create Group</h1>
-          <br />
-          <RoomList user={user} />
-          <Logout />
+    <>
+      <div className="flex flex-col justify-between h-screen w-full max-w-[25vw] lg:max-w-[300px] bg-gray-500">
+        {screen_width < screen_width_threshold && (
+          <button
+            className="top-0 left-0 p-2 h-15 mb-2 bg-gray-800 text-white z-50 w-fit-content"
+            onClick={() => setShow(!show)}
+          >
+            ☰
+          </button>
+        )}
+        {(show || screen_width >= screen_width_threshold) && (
+          <div className="fixed lg:w-[24vw] w-200 max-w-64 h-screen p-5 pt-14 bg-gray-500 z-20">
+            <Search event={createDmRoom} event_name="message" />
+            <br />
+            <RoomList user={user} />
+            <Logout />
+          </div>
+        )}
         </div>
       )}
-    </div>
   );
 }
 
