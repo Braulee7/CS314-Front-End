d26e79f438b9f77f15b1ba2a2373badccc97de88
Braulee7 <braulio00001@gmail.com>
2024-06-04 15:28:58 -0700

    Added new component to create group chats

---
+++ src/components/create-group-form/index.tsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import { useNavigate, useLoaderData } from "react-router-dom";
+import ErrorMessage from "../error-message";
+import Search from "../search";
+import Api from "../../util/api";
+
+interface CreateGroupFormProps {
+  closer: () => void;
+}
+
+function CreateGroupForm(props: CreateGroupFormProps) {
+  const { user } = useLoaderData() as { user: Api };
+  const [members, setMembers] = useState<string[]>([]);
+  const [room_name, setRoomName] = useState<string>("");
+  const [error_message, setErrorMessage] = useState<string>("");
+  const navigate = useNavigate();
+
+  const addMember = (member: string) => {
+    // make sure user is not already in member list
+    if (!members.includes(member)) {
+      setMembers((prev_members) => [...prev_members, member]);
+    }
+  };
+  const createGroup = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const room_id = await user.createGroup(members, room_name);
+      navigate(`/rooms/${room_id}`);
+      props.closer();
+    } catch (e) {
+      const error = e as Error;
+      setErrorMessage(error.message);
+    }
+  };
+  return (
+    <>
+      <ErrorMessage message={error_message} />
+      <div className="flex flex-col align-middle lg:flex-row">
+        <form
+          className="flex flex-col align-middle justify-center bg-gray-500 p-3 rounded-lg w-200 lg:w-300 m-2"
+          onSubmit={createGroup}
+        >
+          <h1> Create a new group</h1>
+          <div className="">
+            <label htmlFor="groupname">Group Name:</label>
+            <input
+              className="border-2 border-gray-300 bg-white h-6 rounded-lg text-sm focus:outline-none"
+              type="text"
+              name="groupname"
+              value={room_name}
+              onChange={(e) => setRoomName(e.target.value)}
+              required
+            />
+          </div>
+          <br />
+          <div>
+            <Search event={addMember} event_name="add to group" />
+          </div>
+          <div>
+            <button className="bg-cyan-950 p-2 text-gray-400 rounded-md">
+              Create Group
+            </button>
+          </div>
+        </form>
+        <div className="bg-gray-500 p-2 rounded-md">
+          <h1 className=" border-b-2 border-gray-950 text-gray-200 font-bold capitalize">
+            Current Group Members
+          </h1>
+
+          <ul className="rounded-md bg-gray-800 text-white max-h-52 w-[100%] max-w-64 overflow-y-scroll scrollbar">
+            {members.map((member) => (
+              <li
+                className="border-b-2 border-cyan-950 text-gray-400 font-bold"
+                key={member}
+              >
+                {member}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CreateGroupForm;
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -3,11 +3,13 @@ import { useLoaderData, useNavigate } from "react-router-dom";
 import Search from "../search";
 import RoomList from "../room-list";
 import Logout from "../logout";
+import CreateGroupForm from "../create-group-form";
 import Api from "../../util/api";
 
 function Navbar() {
   const [show, setShow] = useState(false);
   const [screen_width, setScreenWidth] = useState(window.innerWidth);
+  const [showCGForm, setShowCGForm] = useState(false);
   const { user } = useLoaderData() as { user: Api };
   const nav = useNavigate();
   const screen_width_threshold = 1080;
@@ -48,13 +50,33 @@ function Navbar() {
         {(show || screen_width >= screen_width_threshold) && (
           <div className="fixed lg:w-[24vw] w-200 max-w-64 h-screen p-5 pt-14 bg-gray-500 z-20">
             <Search event={createDmRoom} event_name="message" />
+            <button
+              className="p-2 bg-cyan-950 text-white w-full mt-2"
+              onClick={() => setShowCGForm(!showCGForm)}
+            >
+              Create Group
+            </button>
             <br />
             <RoomList user={user} />
             <Logout />
           </div>
         )}
+      </div>
+      {showCGForm && (
+        <div className="fixed inset-0 flex items-center justify-center z-40">
+          <div className="bg-white rounded-lg p-6 z-50 m-4 max-w-xs mx-auto flex justify-center flex-col lg:max-w-lg">
+            <button
+              className="bg-red-500 p-2 text-gray-200 rounded-md"
+              onClick={() => setShowCGForm(false)}
+            >
+              Close
+            </button>
+            <CreateGroupForm closer={() => setShowCGForm(false)} />
+          </div>
+          <div className="absolute inset-0 bg-black opacity-50"></div>
         </div>
       )}
+    </>
   );
 }
 
